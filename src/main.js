612f5ff35d2a833dc8068fd20c5ec06759e79749
Miketerrara <michael.designer@protonmail.com>
2026-02-04 18:00:54 -0300

    update website, apply gsap

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '../node_modules/@flaticon/flaticon-uicons/css/all/all.css'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+gsap.registerPlugin(ScrollTrigger);
 
-setupCounter(document.querySelector('#counter'))
+gsap.to('.hero__mask-img1', {
+  scrollTrigger: '.hero__mask-img1',
+  y: -600,
+  delay:.2,
+  ease: 'in'
+});
+
+ScrollTrigger.addEventListener("scrollEnd", () =>
+  console.log("scrolling ended!")
+);
